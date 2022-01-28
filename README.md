1f00d632c5bb8a82ab1562d944cb488554237302
Lionel Mason <capitalcookhouse@gmail.com>
2022-01-28 17:30:02 -0500

    Creates Phase 1 NBAProject with balldontlie API

--- README.md
+++ README.md
@@ -1,35 +1,21 @@
-### Project1 - Building A Team
-/Users/charlesgrand/Project1
-charlesgrand@Charless-MBP Project1 % git status
-On branch main
-Your branch and 'origin/main' have diverged,
-and have 1 and 5 different commits each, respectively.
-  (use "git pull" to merge the remote branch into yours)
-
-Changes not staged for commit:
-  (use "git add <file>..." to update what will be committed)
-  (use "git restore <file>..." to discard changes in working directory)
-        modified:   README.md
-        modified:   index.html
-        modified:   index.js
-
-Untracked files:
-  (use "git add <file>..." to include in what will be committed)
-        Project1.json
-        db-1642042199905.json
-        index.css
-
-no changes added to commit (use "git add" and/or "git commit -a")
-charlesgrand@Charless-MBP Project1 % 
-
-### Project1 - Building A Team
+### Project1 - Building Your Roster
 
 ### Overview
-This is a webiste to create your own roster (team) with active players from the NBA.
+Player Card will be used to create a roster (team) that we are keeping track of. We will be able to create a team of both starters and reserves (bench players), and be ble to keep track of certain data very simply (click of buttons).
 
 ### This Criteria used to select these players are as follows:
 
 ### Features
+*Search Player (Submit)
+*Start Player (click)
+*Bench Player (click)
+Hover over Player (mouse over)
+Scroll Through List of Players (scroll bar)
+
+
+
+
+
 *Type players first or last name in (input) and submit to the Form.
 *View Statistics (Pts.,Rebs.,Steals, Assists, etc...)
 *Click Start or Bench to decide your starters and second string player.
--- index.js
+++ index.js
@@ -1,48 +1,163 @@
-function fetchPlayer(input) {
-  fetch(`https://www.balldontlie.io/api/v1/players?search=${input}`)
-    .then((res) => res.json())
-    .then((playerData) => {
-      console.log(playerData)
-      playerData.data.forEach(function (player) {
-        console.log(player)
-       
+/** GLOBAL VARIABLES **/
+  const display = document.querySelector('#display')
+  const searchTerm = document.getElementById('search-input')
+  const submitPlayer= document.getElementById('new-player-search')
+  // const form = document.getElementById('form')
+  const playerContainer= document.getElementById('playercontainer')
+  const players = [];
+  
+
+  
+// function submitForm(e){
+//   e.preventDefault()
+//   const test = document.getElementById('new-player-search').value     //why doesn't submitPlayer work here??
+//   fetchPlayer(test)
+// }
+
+
+
+const createdPlayer = (player) => {
+    const playerDiv = document.createElement('div')
+    const name = document.createElement('h1')
+    name.innerText = player.first_name + ' ' + player.last_name
+    const playerPosition = document.createElement('h3')
+    playerPosition.innerText = player.position
+    const heightFeet = document.createElement('h4')
+    heightFeet.innerText = player.height_feet
+    const heightInches = document.createElement('h4')
+    heightInches.innerText = player.height_inches
+    const weightPounds = document.createElement('h4')
+    weightPounds.innerText = player.weight_pounds
+    const team = document.createElement('h2')
+    team.innerText = player.team.full_name
+    playerDiv.append(name, playerPosition, heightFeet, heightInches, weightPounds, team)
+    const playerContainer = document.getElementById('playercontainer')
+    playerContainer.append(playerDiv)
+}
+
+function renderPlayers(playerData){
+  
+  let fetchedPlayers = playerData.data
+  fetchedPlayers.forEach(player => {
+    // varibale declaration. Interpalate through each object
+  })
+
+}
+
+  const createForm = () => {
+        const formContainer = document.querySelector('#playercontainer')
+        const button1 = document.createElement('button');
+        const button2 = document.createElement('button');
+        
+//         
+        // const form = document.createElement('form');
+
+        /** EVENT HANDLERS **/
+        // formContainer.innerHTML = `
+        // <form id = 'form'>
+        // <input type = "text" placeholder = "Player Name" />
+        // <input type='submit' value='Submit' />
+        // </form>
+        // `
+  }
+
+
+// submitPlayer.addEventListener('submit', submitForm)
+function fetchPlayer(test) {
+  fetch(`https://www.balldontlie.io/api/v1/players?search=${test}`)
+    .then(res => res.json())
+    .then(data => {
+
+  
+      data.data.forEach((player) => {
+        renderPlayer(player);        
       });
     });
 }
-  
- 
-  const createForm = () => {
-        const formContainer = document.querySelector('#playercontainer')
-        
-        
 
-        const button1 = document.createElement('button');
-        const button2 = document.createElement('button');
-        button1.innerText = "START!"
-        button2.innerText = "BENCH!"
-        //const form = document.createElement('form');
-        formContainer.innerHTML = `
-        <form>
-        <input type='text' placeholder='Player Name'>
-        <input type='submit' value='Submit'>
-        </form>
-        `
-        //formContainer.append(form)
-       
-       
-        // button1.setAttribute('id', 'button1')
-        // button2.setAttribute('id', 'button2')
-
-        // document.querySelector('button1').addEventListener('click', e) {
-
-        
+  function renderPlayer(player) {
+    players.push(player)
+    const div = document.createElement('div')
+    div.className = 'playerCard'
+    div.innerHTML = `<p>Player Name: ${player.first_name} ${player.last_name}</p>`
+    const button1 = document.createElement('button');
+    const button2 = document.createElement('button');
+    button1.innerText = "START!"
+    button2.innerText = "BENCH!"
+    button1.addEventListener('click', addToStart)
+    button2.addEventListener('click', addToBench)
+    div.append(button1)
+    div.append(button2)
+    playerContainer.append(div)
   }
 
-  createForm();
+function addToStart (e) {
+ alert ('start')
 
+    let player = e.target.previousElementSibling
+    const startContainer = document.getElementById('starterContainer')
+    starterContainer.append(player);
+    const card = document.querySelector('.playerCard')
+    card.innerHTML= " "
+}
+
+
+function addToBench (e) {
+  alert ('bench')
+ 
+  let player = e.target.previousElementSibling.previousElementSibling
+  const benchContainer = document.getElementById('benchContainer')
+  benchContainer.append(player);
+  const card = document.querySelector('.playerCard')
+  card.innerHTML= " "
+ }
+  
+  
+  submitPlayer.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // playerContainer.innerHTML = " "
+    // const div = document.createElement('div')
+   
+    let searchTerm = document.getElementById('search-input').value
+    playerContainer.innerHTML = " "
+    fetchPlayer(searchTerm)
+  })
+
+
+  const hover = document.getElementById("hover");
+  hover.addEventListener("mouseover", function( event ) {
+    event.target.style.color = "orange";
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+  
+
+  
+  //KEEP LINES 142 - 156???
+  // function startPlayer(e){
+  //   e.preventDefault()
+  //   let starter = document.getElementById('play').value
+  //   // console.log(starter)
+  //   fetchPlayer(starter)
+  // }
+
+  //   function benchPlayer(e){
+  //     e.preventDefault()
+  //     debugger
+  //     let reserve = document.getElementById('sit').value
+  //     // console.log(reserve)
+  //     fetchPlayer(reserve)
+  //   }
+
+
+
+
+  
+  fetchPlayer('');
+  createForm();
+  
+  
   //Need to find form. Need  to .addEventListener to form. 
   //Find a way to get the input value. Pass the value into fetchPlayers
   //Populate the data on the DOM
-
-
-  fetchPlayer('')
